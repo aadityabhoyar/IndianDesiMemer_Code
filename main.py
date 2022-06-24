6fdc2a6febbb41913b232ef5ed9f0d2b05b3bfa2
Aditya Bhoyar <103208363+MrAdityaBhoyar@users.noreply.github.com>
2022-06-24 18:43:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def help(ctx):
         "🤗 `emotion   :` Share Your Emotion\n" +
         "🌚 `fun       :` Entertainment! **Bus Thoda Sambhal Ke**\n" +
         "🔧 `utility   :` Bot utility zone\n\n" +
-        "To view the commands of a category, send `yar help <category>`\n\n[Add the Bot](https://discord.com/api/oauth2/authorize?client_id=986632998503997500&permissions=122406824960&scope=bot) - [Need Support?](https://discord.com/api/oauth2/authorize?client_id=986632998503997500&permissions=122406824960&scope=bot) - [Join the Community](https://discord.com/api/oauth2/authorize?client_id=986632998503997500&permissions=122406824960&scope=bot)",
+        "To view the commands of a category, send `yar help <category>`\n\n[Add the Bot](https://discord.com/api/oauth2/authorize?client_id=986632998503997500&permissions=122406824960&scope=bot))",
         inline=False)
     embed.set_footer(icon_url=ctx.author.avatar_url,
                      text="Help requested by: {}".format(
