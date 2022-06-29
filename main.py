2616feac14436cfecbb899141219aad11e1a2541
Aditya Bhoyar <103208363+MrAdityaBhoyar@users.noreply.github.com>
2022-06-29 20:37:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -893,7 +893,7 @@ async def guildlink(ctx):
   await ctx.send("💝| https://discord.gg/saAEtYRs")
 
     
- @client.command()
+@client.command()
 async def si(ctx):
   print(f"Server: {guild.name}")  
     
