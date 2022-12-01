be4b161b5919808a84708b4c699eed52406cc0a6
Aditya Bhoyar <103208363+MrAdityaBhoyar@users.noreply.github.com>
2022-12-02 02:08:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def help(ctx):
     embed = discord.Embed(
         title="IndianDesiMemer Help Center ✨",
         url=
-        "https://discord.com/api/oauth2/authorize?client_id=987391836886794260&permissions=122406693952&scope=bot",
+        "https://discord.com/oauth2/authorize?client_id=987391836886794260&permissions=517543905344&scope=bot%20applications.commands",
         color=0xF49726)
     embed.add_field(
         name="Command Categories :",
